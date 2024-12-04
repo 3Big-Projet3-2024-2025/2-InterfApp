1510fba70eea6aceb61af7dbc709958622d93ee9
Dedecker Bastien <127665643+la228797@users.noreply.github.com>
2024-12-04 11:42:14 +0100

    AddIng test and javadoc
    Adding test for mongoConfig

--- Backend/src/main/java/be/helha/interf_app/config/MongoConfig.java
+++ Backend/src/main/java/be/helha/interf_app/config/MongoConfig.java
@@ -6,14 +6,29 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 
+/**
+ * Configuration class for connecting to a MongoDB database.
+ * This class sets up the MongoDB client and defines the database name to be used.
+ * It extends {@link AbstractMongoClientConfiguration} to leverage Spring Data MongoDB configurations.
+ */
 @Configuration
 public class MongoConfig extends AbstractMongoClientConfiguration {
 
+    /**
+     * Specifies the name of the MongoDB database to be used.
+     *
+     * @return the name of the database, "interfApp".
+     */
     @Override
-    protected String getDatabaseName(){
+    public String getDatabaseName(){
         return "interfApp"; // Name of the database
     }
 
+    /**
+     * Creates and configures a {@link MongoClient} bean for connecting to MongoDB.
+     *
+     * @return a {@link MongoClient} instance pointing to "mongodb://localhost:27017".
+     */
     @Bean
     @Override
     public MongoClient mongoClient() {
