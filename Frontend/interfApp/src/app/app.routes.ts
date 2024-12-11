b10ded3838f9509e76286d6a0bf78bf4f0d7c654
Dedecker Bastien <127665643+la228797@users.noreply.github.com>
2024-12-11 16:27:25 +0100

    Adding Profil
    Adding profil page and router

--- Frontend/interfApp/src/app/app.routes.ts
+++ Frontend/interfApp/src/app/app.routes.ts
@@ -6,12 +6,14 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { authGuardGuard } from './guard/auth-guard.guard';
 import { roleGuardGuard } from './guard/role-guard.guard';
+import { ProfilComponent } from './profil/profil.component';
 
 export const routes: Routes = [
     { path: 'createform', component: CreateFormComponent, canActivate: [authGuardGuard, roleGuardGuard], data: { role: 'ROLE_User' }},
     { path: '', redirectTo: '/login', pathMatch: 'full' },
     { path: 'forms', component: OpenFormComponent , canActivate: [authGuardGuard, roleGuardGuard], data: { role: 'ROLE_User' }},
     { path: 'form/:id', component:  ReplyFormComponent, canActivate: [authGuardGuard, roleGuardGuard], data: { role: 'ROLE_User' }},
+    { path: 'profil', component:  ProfilComponent, canActivate: [authGuardGuard, roleGuardGuard], data: { role: 'ROLE_User' }},
     { path: 'register', component:  RegisterComponent},
     { path: 'login', component:  LoginComponent},
 ];
