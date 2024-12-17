f3d243718cd05016d9c7b8ca3fe821a6c688081e
Dedecker Bastien <127665643+la228797@users.noreply.github.com>
2024-12-17 16:50:58 +0100

    Fix email functionality
    realization of tests for mails
    correction of bugs related to mails
    addition of javadoc for mails

--- Backend/src/main/java/be/helha/interf_app/Controller/EmailController.java
+++ Backend/src/main/java/be/helha/interf_app/Controller/EmailController.java
@@ -8,14 +8,34 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
-
+/**
+ * Controller class for handling email-related API requests.
+ * This class provides an endpoint to send emails via the EmailService.
+ *
+ * Endpoints:
+ * - POST /api/mail/send: Sends an email using the provided EmailDetails.
+ */
 @RestController
 @RequestMapping("/api/mail")
 @CrossOrigin(origins = "*")
 public class EmailController {
+    /**
+     * The service used to handle email sending operations.
+     */
     @Autowired
     private EmailService emailService;
-
+    /**
+     * Endpoint to send an email with the details provided in the request body.
+     *
+     * This method receives an EmailDetails object from the request body,
+     * and uses the EmailService to send the email. It returns a response with
+     * the result of the operation.
+     *
+     * @param emailDetails The details of the email to be sent, including recipient, subject, and message.
+     * @return A ResponseEntity containing the status and a message:
+     *         - If email sending is successful, a 200 OK response with the success message.
+     *         - If there is an error, a 500 Internal Server Error response with the error message.
+     */
     @PostMapping("/send")
     public ResponseEntity<?> sendMail(@RequestBody EmailDetails emailDetails) {
         String response = emailService.sendSimpleMail(emailDetails);
--- Backend/src/main/java/be/helha/interf_app/Model/EmailDetails.java
+++ Backend/src/main/java/be/helha/interf_app/Model/EmailDetails.java
@@ -3,12 +3,26 @@ package be.helha.interf_app.Model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+/**
+ * Model class representing the details of an email.
+ * This class contains the recipient's email address, the subject of the email,
+ * and the message content to be sent.
+ *
+ */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class EmailDetails {
+    /**
+     * The email address of the recipient.
+     */
     private String recipient;
+    /**
+     * The subject of the email.
+     */
     private String subject;
+    /**
+     * The content of the email message.
+     */
     private String message;
 }
