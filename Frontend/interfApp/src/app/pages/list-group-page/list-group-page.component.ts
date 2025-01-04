8240b80838f36600df99ca6799e1f685e0aa21ef
Collart Clara <la226237@student.helha.be>
2025-01-04 19:22:05 +0100

    creating search bar for groups
    created a search bare. User can now search groups by their name

--- Frontend/interfApp/src/app/pages/list-group-page/list-group-page.component.ts
+++ Frontend/interfApp/src/app/pages/list-group-page/list-group-page.component.ts
@@ -3,18 +3,23 @@ import { GroupService } from '../../services/group.service';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { UserService } from '../../services/user.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-list-group-page',
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule, FormsModule],
   templateUrl: './list-group-page.component.html',
   styleUrl: './list-group-page.component.css'
 })
 export class ListGroupPageComponent {
   groups: any[] = [];
+  filteredGroups: any[] = []; // filtered list by research
   user:any[]=[];
   currentPage: number = 1; // actual page
   groupPerPage: number = 9; // number of group per page
+  searchQuery: string = ''; // research querry 
+  noResultsFound: boolean = false; // Indicateur d'absence de résultats
 
   constructor(private groupService: GroupService, private userService: UserService) {}
 
@@ -22,6 +27,7 @@ export class ListGroupPageComponent {
     this.groupService.getAllGroups().subscribe(
       (data) => {
         this.groups = data;
+        this.filteredGroups = [...this.groups]; // initialise groups
       },
       (error) => {
         console.error('Erreur lors de la récupération des groupes', error);
@@ -32,7 +38,7 @@ export class ListGroupPageComponent {
   // to separage the groups of each page
   getPaginatedGroups(): any[] {
     const startIndex = (this.currentPage - 1) * this.groupPerPage;
-    return this.groups.slice(startIndex, startIndex + this.groupPerPage);
+    return this.filteredGroups.slice(startIndex, startIndex + this.groupPerPage);
   }
 
 
@@ -62,4 +68,28 @@ export class ListGroupPageComponent {
     return Math.ceil(this.groups.length / this.groupPerPage);
   }
 
+   // Filtrer les groupes sur clic du bouton
+  filterGroups(): void {
+    if (this.searchQuery.trim() === '') {
+      // Si la recherche est vide, afficher tous les groupes
+      this.filteredGroups = [...this.groups];
+    }
+    else {
+      // Filtrer les groupes
+      this.filteredGroups = this.groups.filter((group) =>
+        group.name.toLowerCase().includes(this.searchQuery.toLowerCase())
+      );
+    }
+    this.currentPage = 1; // Réinitialiser à la première page
+     // Vérifier si des groupes ont été trouvés
+     if (this.filteredGroups.length === 0) {
+      this.noResultsFound = true; // Mettre à jour la variable pour indiquer qu'il n'y a pas de résultats
+    } else {
+      this.noResultsFound = false; // Réinitialiser si des résultats sont trouvés
+    }
+
+    this.currentPage = 1; // Réinitialiser à la première page après filtrage
+  }
+
+  
 }
