dec7ff5071ff31955c6a572c67b15c8ea4627e9e
Collart Clara <la226237@student.helha.be>
2025-01-04 14:24:15 +0100

    creating a pagination for the group
    created a pagination to show all the group, and function to get the managar by their name instead of their id

--- Frontend/interfApp/src/app/pages/list-group-page/list-group-page.component.ts
+++ Frontend/interfApp/src/app/pages/list-group-page/list-group-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { GroupService } from '../../services/group.service';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-list-group-page',
@@ -11,8 +12,11 @@ import { RouterModule } from '@angular/router';
 })
 export class ListGroupPageComponent {
   groups: any[] = [];
+  user:any[]=[];
+  currentPage: number = 1; // actual page
+  groupPerPage: number = 9; // number of group per page
 
-  constructor(private groupService: GroupService) {}
+  constructor(private groupService: GroupService, private userService: UserService) {}
 
   ngOnInit(): void {
     this.groupService.getAllGroups().subscribe(
@@ -20,8 +24,42 @@ export class ListGroupPageComponent {
         this.groups = data;
       },
       (error) => {
-
+        console.error('Erreur lors de la récupération des groupes', error);
       }
     );
   }
+  
+  // to separage the groups of each page
+  getPaginatedGroups(): any[] {
+    const startIndex = (this.currentPage - 1) * this.groupPerPage;
+    return this.groups.slice(startIndex, startIndex + this.groupPerPage);
+  }
+
+
+ /* getUserName(managerId:any): any{
+    if (managerId) {
+      this.userService.getUserById(managerId).subscribe(
+        (user) => {
+          let nameManager = user.username; 
+          return nameManager;
+        },
+        (error) => {
+          console.error(`Erreur lors de la récupération de l'utilisateur avec l'id ${managerId}`, error);
+        }
+      );
+    }
+  }
+*/
+
+  goToPage(page:number){
+    if (page > 0 && page <= this.totalPages()) {
+      this.currentPage = page;
+    }
+  }
+
+  // calculate de number of pages 
+   totalPages(): number {
+    return Math.ceil(this.groups.length / this.groupPerPage);
+  }
+
 }
